e37ae03771949205a6c88c2eb2b67f451d05732c
Harini-Sri-Vedha-Alvar <harinisva09@gmail.com>
2026-03-09 21:06:53 +0530

    Initial commit - campus canteen app

--- food-delivery-backend/server.js
+++ food-delivery-backend/server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // backend/server.js
 const express = require('express');
 const mongoose = require('mongoose');
@@ -33,20 +32,4 @@ mongoose.connect(process.env.MONGO_URI, {
 // Start server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
-=======
-const express = require("express");
-const cors = require("cors");
-const pool = require("./db");
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-const userRoutes = require("./routes/users");
-app.use("/users", userRoutes);
-
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
->>>>>>> 587430f764b0d743fb39fb92d3ba5f589cc22160
 });
